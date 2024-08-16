37dc16713cab3a47292307e0340892095de23d87
michelebswm <michele.fenix.17@gmail.com>
2024-08-15 21:28:56 -0300

    feat: conftest para zerar o banco a cada test e criacao de tests de conflict

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,22 +2,46 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from madr.app import app
-from madr.models import table_registry
+from madr.database import get_session
+from madr.models import Conta, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:', connect_args={'check_same_thread': False}, poolclass=StaticPool
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
         yield session
 
     table_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def user(session):
+    pwd = 'testtest'
+    conta = Conta(username='Teste', email='teste@test.com', senha=pwd)
+    session.add(conta)
+    session.commit()
+    session.refresh(conta)
+
+    conta.clean_password = pwd
+
+    return conta
--- tests/test_conta.py
+++ tests/test_conta.py
@@ -3,6 +3,21 @@ from http import HTTPStatus
 
 def test_create_conta_ok(client):
     response = client.post(
-        '/conta', json={'username': 'test', 'email': 'test@test.com', 'senha': '12345'}
+        '/conta', json={'username': 'testusername', 'email': 'testemail@test.com', 'senha': '12345'}
     )
     assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {'username': 'testusername', 'email': 'testemail@test.com', 'id': 1}
+
+
+def test_create_conta_conflict_username(client, user):
+    response = client.post(
+        '/conta', json={'username': user.username, 'email': 'test@test.com', 'senha': '12345'}
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+
+
+def test_create_conta_conflict_email(client, user):
+    response = client.post(
+        '/conta', json={'username': 'testeusername', 'email': user.email, 'senha': '12345'}
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
